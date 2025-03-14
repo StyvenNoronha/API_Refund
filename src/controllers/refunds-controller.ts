968e14f0c2108a88ad9b9773ffd9b92eabe05d5f
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 13:45:01 -0300

    Validação de dados da solicitação

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,7 +1,23 @@
+import { Category } from "@prisma/client";
 import { Request, Response } from "express";
+import { z } from "zod";
 
-export class RefundsController{
-    async create(request: Request, response: Response){
-        response.json({message: "ok"})
-    }
+export class RefundsController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
+      category: z.enum([
+        Category.food,
+        Category.others,
+        Category.services,
+        Category.transport,
+        Category.accommodation,
+      ]),
+      amount: z.number().positive({ message: "O valor precisa ser positivo" }),
+      filename: z.string().min(10),
+    });
+
+    const { name, category, amount, filename } = bodySchema.parse(request.body);
+    response.json({ message: "ok" });
+  }
 }
