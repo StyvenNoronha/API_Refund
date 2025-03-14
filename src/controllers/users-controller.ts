b1d209a83a74eda86b9aa139a3e6f60cdd502bc9
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 10:11:31 -0300

    Cadastrando usuário no banco

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,18 +1,47 @@
 import { Request, Response } from "express";
 import { UserRole } from "@prisma/client";
-import {z} from "zod"
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+import { AppError } from "@/utils/AppError";
+import { hash } from "bcrypt";
 
+export class UsersController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: "Nome é obrigatório" }),
+      email: z
+        .string()
+        .trim()
+        .email({ message: "E-mail inválido" })
+        .toLowerCase(),
+      password: z
+        .string()
+        .min(6, { message: "a senha deve ter no mínimo 6 dígitos" }),
+      role: z
+        .enum([UserRole.employee, UserRole.manager])
+        .default(UserRole.employee),
+    });
 
-export class UsersController{
-    async create(request: Request, response:Response){
-        const bodySchema = z.object({
-            name: z.string().trim().min(2,{message:"Nome é obrigatório"}),
-            email: z.string().trim().email({message:"E-mail inválido"}).toLowerCase(),
-            password: z.string().min(6,{message:"a senha deve ter no mínimo 6 dígitos"}),
-            role:z.enum([UserRole.employee, UserRole.manager]).default(UserRole.employee)
-        })
-
-        const {name, email, password, role} = bodySchema.parse(request.body)
-        response.json({name, email, password, role})
+    const { name, email, password, role } = bodySchema.parse(request.body);
+    //verifica se ja ter email cadastrado
+    const UserWithSameEmail = await prisma.user.findFirst({ where: { email } });
+    if (UserWithSameEmail) {
+      throw new AppError(" já existe um usuário cadastrado com esse E-mail");
     }
+
+    //criptografar a senha
+    const hashedPassword = await hash(password, 8);
+
+    //salvar no banco de dados
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role,
+      },
+    });
+
+    response.status(201).json({ message: "Usuário criado com sucesso" });
+  }
 }
