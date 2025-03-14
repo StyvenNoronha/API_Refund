1172c2a3c0a5004c7354b917c6a510b750ca2e7a
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 08:55:22 -0300

    Tratamento de exceções

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import  express  from "express";
 import cors from "cors"
+import { errorHandling } from "./middlewares/error-handling";
+import { AppError } from "./utils/AppError";
 
 const app = express()
 app.use(cors())
@@ -7,4 +9,6 @@ app.use(express.json())
 app.get('/',(req,res)=>{
     res.send("Hello World")
 })
+
+app.use(errorHandling)
 export {app}
