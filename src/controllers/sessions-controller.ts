0287a1a7cb7307f0f1bf560d03c9b18499a02a5c
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 10:34:28 -0300

    Validando dados da sessions

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,6 +1,12 @@
+import {z} from"zod"
 import { Request, Response } from "express";
 export class SessionsController{
     async create(request: Request, response: Response){
-        response.json({message:"ok"})
+        const bodySchema = z.object({
+            email: z.string().email({message:"E-mail inválido"}),
+            password:z.string()
+        })
+        const {email,password} = bodySchema.parse(request.body)
+        response.json({email, password})
     }
 }
