388191dfc1e830846656e497932890f4c6453e37
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 11:08:04 -0300

    Controller e Rota de refunds

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { usersRoutes } from "./users-routes";
 import { sessionsRoutes } from "./sessions-routes";
+import { refundsRoutes } from "./refunds-routes";
 const routes = Router()
 
 
 //rotas Publicas
 routes.use('/users', usersRoutes)
 routes.use('/sessions', sessionsRoutes)
+
+
+
+//Rotas privadas
+routes.use('/refunds', refundsRoutes)
 export{routes}
