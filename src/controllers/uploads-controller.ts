38ad8f22409eb3227ff7fc7f8057f5520d974c0e
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 16:49:02 -0300

    Validando arquivo

--- src/controllers/uploads-controller.ts
+++ src/controllers/uploads-controller.ts
@@ -1,6 +1,25 @@
-import { json, Request, Response } from "express";
+import { Request, Response } from "express";
+import z from "zod";
+
+import uploadConfig from "@/configs/uploadConfig";
+
 export class UploadsController {
   async create(request: Request, response: Response) {
-    response.json({ message: "ok" });
+    try {
+      const fileSchema = z.object({
+        filename: z.string().min(1, "arquivo é obrigatório"),
+        mimetype: z
+          .string()
+          .refine(
+            (type) => uploadConfig.ACCEPTED_IMAGE_TYPES.includes(type),
+            "formato de arquivo invalido"
+          ),
+          size: z.number().positive().refine((size)=> size <=  uploadConfig.MAX_FILE_SIZE,"Arquivo muito grande")
+      }).passthrough()
+      const {file} = fileSchema.parse(request.file)
+      response.json({message:"ok"})
+    } catch (error) {
+      throw error;
+    }
   }
 }
