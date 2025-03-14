233ff9118f49a341a10d30c96d1f5fda8a843c7a
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 14:02:45 -0300

    Cadastrando solicitação

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,23 +1,40 @@
 import { Category } from "@prisma/client";
 import { Request, Response } from "express";
 import { z } from "zod";
-
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+const CategoriaEnum = z.enum([
+    "food",
+    "others",
+    "services",
+    "transport",
+    "accommodation"
+])
 export class RefundsController {
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
       name: z.string().trim().min(1, { message: "Nome é obrigatório" }),
-      category: z.enum([
-        Category.food,
-        Category.others,
-        Category.services,
-        Category.transport,
-        Category.accommodation,
-      ]),
+      category: CategoriaEnum,
       amount: z.number().positive({ message: "O valor precisa ser positivo" }),
-      filename: z.string().min(10),
+      fileName: z.string(),
     });
 
-    const { name, category, amount, filename } = bodySchema.parse(request.body);
-    response.json({ message: "ok" });
+    const { name, category, amount, fileName } = bodySchema.parse(request.body);
+
+    if(!request.user?.id){
+        throw new AppError("Usuário não autorizado", 401)
+    }
+
+    const refund = await prisma.refunds.create({
+        data:{
+            name,
+            amount,
+            category,
+            fileName,
+            userId: request.user.id,
+
+        }
+    })
+    response.status(201).json({ refund });
   }
 }
