1abfb20e4ed7a95fefbf678da97afa05a8e841b6
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 14:27:23 -0300

    Listando solicitações

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -4,12 +4,12 @@ import { z } from "zod";
 import { prisma } from "@/database/prisma";
 import { AppError } from "@/utils/AppError";
 const CategoriaEnum = z.enum([
-    "food",
-    "others",
-    "services",
-    "transport",
-    "accommodation"
-])
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accommodation",
+]);
 export class RefundsController {
   async create(request: Request, response: Response) {
     const bodySchema = z.object({
@@ -21,40 +21,39 @@ export class RefundsController {
 
     const { name, category, amount, fileName } = bodySchema.parse(request.body);
 
-    if(!request.user?.id){
-        throw new AppError("Usuário não autorizado", 401)
+    if (!request.user?.id) {
+      throw new AppError("Usuário não autorizado", 401);
     }
 
     const refund = await prisma.refunds.create({
-        data:{
-            name,
-            amount,
-            category,
-            fileName,
-            userId: request.user.id,
-
-        }
-    })
+      data: {
+        name,
+        amount,
+        category,
+        fileName,
+        userId: request.user.id,
+      },
+    });
     response.status(201).json({ refund });
   }
 
-  async index(request: Request, response: Response){
+  async index(request: Request, response: Response) {
     const querySchema = z.object({
-        name: z.string().optional().default(""),
-    })
+      name: z.string().optional().default(""),
+    });
 
-    const {name} = querySchema.parse(request.query)
+    const { name } = querySchema.parse(request.query);
     const refunds = await prisma.refunds.findMany({
-        where:{
-            user:{
-                name:{
-                    contains:name.trim()
-                }
-            }
+      where: {
+        user: {
+          name: {
+            contains: name.trim(),
+          },
         },
-        orderBy:{createdAt:"desc"},
-        include:{user:true}
-    })
-    response.json({refunds})
+      },
+      orderBy: { createdAt: "desc" },
+      include: { user: true },
+    });
+    response.json({ refunds });
   }
 }
