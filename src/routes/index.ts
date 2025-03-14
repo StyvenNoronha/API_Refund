a67ed75c0cad0233eab8db6c99410defabd64a6c
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 10:30:24 -0300

    Criando controller e rota de autenticação

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { usersRoutes } from "./users-routes";
-
+import { sessionsRoutes } from "./sessions-routes";
 const routes = Router()
 
 
 //rotas Publicas
 routes.use('/users', usersRoutes)
-
+routes.use('/sessions', sessionsRoutes)
 export{routes}
