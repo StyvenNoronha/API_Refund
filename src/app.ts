df5d31745b13d77f09413eeec9333eb03bb46b3b
Styven Noronha <98356877+StyvenNoronha@users.noreply.github.com>
2025-03-14 09:03:41 -0300

    Instalando e configurando o Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,10 @@
-import  express  from "express";
-import cors from "cors"
+import express from "express";
+import cors from "cors";
 import { errorHandling } from "./middlewares/error-handling";
-import { AppError } from "./utils/AppError";
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send("Hello World")
-})
+const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.use(errorHandling)
-export {app}
+app.use(errorHandling);
+export { app };
